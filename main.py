7d06a4cc65791c8ee2eba1fff52d4d4fac81f36a
peter-perger <pergerpeti2@gmail.com>
2026-03-05 13:45:01 +0100

    Refactor app route to support dynamic parameters

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask("website")
 app.json.sort_keys = False
+
+
+# df = pd.read_csv("")
 
 
 @app.route('/')
@@ -11,10 +16,21 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
-    return {"station": station,
-            "date": date,
-            "temperature": temperature}
+    station_string = str(station).zfill(6)
+    path = f"data_small\TG_STAID{station_string}.txt"
+
+    df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
+
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
+    print(temperature)
+
+    result_dict = {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+
+    return result_dict
 
 
 if __name__ == "__main__":
