e307739c1ce9c8d5e4163efcb45e7751d2a6ca6c
peter-perger <pergerpeti2@gmail.com>
2026-03-07 23:02:59 +0100

    Final touches

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask("website")
 app.json.sort_keys = False
 
 
-stations = pd.read_csv("data_small\stations.txt", skiprows=17)
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
 
 
@@ -18,7 +18,7 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     station_string = str(station).zfill(6)
-    path = f"data_small\TG_STAID{station_string}.txt"
+    path = f"data_small/TG_STAID{station_string}.txt"
 
     df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
 
@@ -36,7 +36,7 @@ def about(station, date):
 @app.route("/api/v1/<station>")
 def all_data(station):
     station_string = str(station).zfill(6)
-    path = f"data_small\TG_STAID{station_string}.txt"
+    path = f"data_small/TG_STAID{station_string}.txt"
 
     df = pd.read_csv(path, skiprows=20, parse_dates=["    DATE"])
     result = df.to_dict(orient="records")
@@ -47,13 +47,11 @@ def all_data(station):
 @app.route("/api/v1/yearly/<station>/<year>")
 def yearly(station, year):
     station_string = str(station).zfill(6)
-    path = f"data_small\TG_STAID{station_string}.txt"
+    path = f"data_small/TG_STAID{station_string}.txt"
 
     df = pd.read_csv(path, skiprows=20)
     df['    DATE'] = df['    DATE'].astype(str)
     result = df[df["    DATE"].str.startswith(str(year))]
-
-    print(result)
 
     return result.to_dict(orient="records")
 
